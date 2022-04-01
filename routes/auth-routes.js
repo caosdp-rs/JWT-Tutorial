ea98ece3aa7199a68d4933781965f5605f8e17fd
caosdp-rs <caosdp@gmail.com>
2022-04-01 02:15:05 -0300

    token jwt delete

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -16,11 +16,39 @@ router.post('/login',async(req,res)=>{
         if (!validPassword) return res.status(401).json({error: "Email is incorrect or password invalid"})
         //return res.status(200).json("Success");
         //JWT
+        let tokens = jwtTokens(users.rows[0]);
+        res.cookie('refresh_token',tokens.refreshToken,{httpOnly:true});
+        res.json(tokens);
+
     }catch(error){
-        res.status(500).json({error:error.message});
+        res.status(401).json({error:error.message});
     }
 });
 
+router.get('/refresh_token',(req,res)=>{
+    try {
+        const refreshToken = req.cookies.refresh_token;
+        if(refreshToken === null) return res.status(401).json({error:'Null refresh token'});
+        jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET,(error,user)=>{
+            if(error) return res.status(403).json({error:error.message});
+            let tokens = jwtTokens(user);
+            res.cookie('refresh_token',tokens.refreshToken,{httpOnly:true});
+            res.json(tokens);
+        })
+    } catch (error) {
+        res.status(401).json({error:error.message});
+    }
+})
+
+
+router.delete('/refresh_token',(req,res)=>{
+    try {
+        res.clearCookie('refresh_token');
+        return res.status(200).json({message:'refresh token deleted.'})
+    } catch (error) {
+        res.status(401).json({error:error.message});
+    }
+})
 
 
 
